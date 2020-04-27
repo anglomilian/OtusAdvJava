e7565983cfef96497d90acb6946666459df7e2ae
anglomilian <anglomilian@gmail.com>
2020-04-27 14:45:00 +0300

    Второй этап испытания GC на большем объеме Heap

--- hw07-GC/src/main/java/ru/otus/OutOfMemorer.java
+++ hw07-GC/src/main/java/ru/otus/OutOfMemorer.java
@@ -5,20 +5,23 @@ import java.util.ArrayList;
 import static ru.otus.RandomString.*;
 
 public class OutOfMemorer {
-    private ArrayList <String> bigLibrary = new ArrayList<>();
+    private ArrayList <Book> bigLibrary = new ArrayList<>();
     private final int ITEMS_TO_ADD = 10000;
 
     public void fillLabrary() throws InterruptedException {
+        int i = 0;
         while (true){
             addBooks();
             removeBooks();
-            Thread.sleep(20);
+            Thread.sleep(50);
+            System.out.println("Fill library counter: " + i++);
         }
     }
 
     private void addBooks(){
         for (int i = 0; i < ITEMS_TO_ADD; i++){
-            bigLibrary.add(getRandomString(20));
+            //bigLibrary.add(getRandomString(20));
+            bigLibrary.add(new Book(getRandomString(15), getRandomString(20), Math.random() * 100));
         }
     }
 
@@ -26,5 +29,30 @@ public class OutOfMemorer {
         for (int i = 0; i < ITEMS_TO_ADD / 4; i++) {
             bigLibrary.remove(i);
         }
+    }
+}
+
+class Book{
+    private String name;
+    private String author;
+    private double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    Book(String n, String a, double p){
+        name = n;
+        author = a;
+        price = p;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("\tBook name: %s, Author: %s, Price: %.02f" + System.lineSeparator(), name, author, price);
     }
 }
